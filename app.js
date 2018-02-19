a8378af03439ed7b573b3be0725f4d107e2d1e01
xn-- -726az9hu87f <izuizu_9@hotmail.com>
2018-02-19 09:05:57 +0000

    web-configフォントmime設定

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var app = express();
 
 // Add a simple route for static content served from 'public'
 app.use("/",express.static("public"));
-app.use("/materialize-css",express.static("public"));
 
 // Create a server
 var server = http.createServer(app);
